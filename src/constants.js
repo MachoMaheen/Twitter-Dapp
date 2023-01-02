d5ecbec7d7c0aa7462fd8b61ccae1fb8db4a56ce
MachoMaheen <kabeerajmal6@gmail.com>
2023-01-02 09:02:26 +0530

    Add Twitter Dapp code

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const APP_CONSTANTS = {
+  ALCHEMY_KEY:
+    " wss://powerful-lingering-pond.matic-testnet.discover.quiknode.pro/79b37692b9cd7ddd116599176100dff106451926/",
+
+  CONTRACT_ADDRESS: "0x6d4eE43f7902a110932a0d5a0cBE1A751ddBDdC9",
+
+  OPENSEA_ASSETS_URL: "https://testnets.opensea.io/assets/mumbai",
+
+  CLIENT_ID:
+    "BDabQ0CjKtqZlTUiPJFHkvqHJXwodANmGPxrnyi3lkC6bOD6v9trBS0v82ZcSRnKk-4uTd6NjsBOLCKLKJRAd_g",
+
+  RPC_TARGET:
+    "https://powerful-lingering-pond.matic-testnet.discover.quiknode.pro/79b37692b9cd7ddd116599176100dff106451926/",
+
+  APP_LOGO: "https://i.ibb.co/2gmPw9f/twitter-Dapp-Logo.jpg",
+
+  ADAPTER_TWITTER_CLIENT_VERIFIER: "twitter-macho-dapp",
+
+  ADAPTER_TWITTER_CLIENT_ID: "PSe9PWE1WuWYQEFT3FD8P6Vvdlvq0lo5",
+
+  AUTH0_DOMAIN: "https://dev-8o2e7f3m.us.auth0.com",
+
+  BIRD_IMAGE: "https://i.ibb.co/42WNxRN/reshot-icon-bird-TL39-D5-P8-YH.png",
+
+  REACT_APP_REFRESH_TIMER: 30,
+};
